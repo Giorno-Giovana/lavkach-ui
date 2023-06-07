cf70fcb39024017df980dc00fee8dd4e598f0d02
Zaver <be9st@ya.ru>
2023-06-07 14:35:07 +0300

    Пройден гайд по иниту

---
+++ .app/pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+definePageMeta({
+  title: 'My page',
+})
+</script>
+
+<template>
+  Hello Tairo!
+</template>
--- .app/tailwind.config.ts
+++ .app/tailwind.config.ts
@@ -1,12 +1,6 @@
 import { withShurikenUI } from '@shuriken-ui/tailwind'
 import colors from 'tailwindcss/colors'
 
-/**
- * This is the Tailwind config file for the demo.
- * It extends the default config from @shuriken-ui/tailwind
- *
- * You can add/override your own customizations here.
- */
 export default withShurikenUI({
   content: [],
   theme: {
@@ -29,12 +23,12 @@ export default withShurikenUI({
     extend: {
       // Custom colors
       colors: {
-        // primary: colors.indigo,
-        // muted: colors.slate,
-        // info: colors.sky,
-        // success: colors.teal,
-        // warning: colors.amber,
-        // danger: colors.rose,
+        primary: colors.indigo,
+        muted: colors.slate,
+        info: colors.sky,
+        success: colors.teal,
+        warning: colors.amber,
+        danger: colors.rose,
       },
     },
   },
