8f123155d3b5ac6ea64cef336d4a0f9729470e36
viktor-shved <viktor-shved@yandex-team.ru>
2023-06-09 15:52:25 +0300

    Витя облагустроил

--- .demo/pages/assets.vue
+++ .demo/pages/assets.vue
@@ -1,14 +1,8 @@
 <template>
-  <TairoContentWrapper class="flex flex-col gap-8">
-    <div v-if="data === null || !data.length">loading</div>
-    <Asset v-else :asset="a" v-for="a in data" :key="a.id" />
-    <!-- <TairoTable shape="curved">
+     <TairoTable shape="curved">
       <template #header>
-        <TairoTableHeading uppercase class="pl-4"
-          >Наименование</TairoTableHeading
+        <TairoTableHeading uppercase class="pl-4">Наименование</TairoTableHeading
         >
-        <TairoTableHeading uppercase>id</TairoTableHeading>
-        <TairoTableHeading uppercase>LSN</TairoTableHeading>
         <TairoTableHeading uppercase>Тип</TairoTableHeading>
         <TairoTableHeading uppercase>Сейчас у</TairoTableHeading>
         <TairoTableHeading uppercase>Серийный номер</TairoTableHeading>
@@ -21,21 +15,18 @@
 
       <TairoTableRow v-for="d in data" :key="d.id">
         <TairoTableCell class="pl-4">{{ d.title }}</TairoTableCell>
-        <TairoTableCell light>{{ d.id }}</TairoTableCell>
-        <TairoTableCell>{{ d.lsn }}</TairoTableCell>
         <TairoTableCell>{{ d.asset_type.title }}</TairoTableCell>
         <TairoTableCell>{{ d.at }}</TairoTableCell>
         <TairoTableCell>{{ d.serial }}</TairoTableCell>
         <TairoTableCell>{{ d.manufacturer.title }}</TairoTableCell>
         <TairoTableCell>{{ d.status }}</TairoTableCell>
         <TairoTableCell>{{ d.store.title }}</TairoTableCell>
-        <TairoTableCell>{{ d.user.nickname }}</TairoTableCell>
+        <TairoTableCell>{{ d.user_created_id.nickname }}</TairoTableCell>
         <TairoTableCell>
           <BaseButtonAction> Открыть </BaseButtonAction>
         </TairoTableCell>
       </TairoTableRow>
-    </TairoTable> -->
-  </TairoContentWrapper>
+    </TairoTable>
 </template>
 
 <script setup lang="ts">
